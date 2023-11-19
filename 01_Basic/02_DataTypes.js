a72cc057cdf622042fac56645952f2e35b99ac45
D2-CODE <148619148+D2-CODE@users.noreply.github.com>
2023-11-19 10:59:38 +0000

    datatypes of js and typeof

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,24 @@
+//data types in js
+//we are not define type like number int string boolean js automatic detact that
+
+let Pname= "Dharmesh";//it is string type
+
+let age = 19;//it is number type
+
+let genderMale=true;//it is boolean type
+
+let mobileNumber;//it is undefined type its value is also undefined
+
+let bnakBalance=null;//it is null type value it carray null ,null is asign
+
+
+console.table([Pname,age,genderMale,mobileNumber,bnakBalance]);
+
+//we have keyword or method of js thats tell us type of variables
+
+console.table([typeof Pname,typeof age,typeof genderMale,typeof mobileNumber,typeof bnakBalance]);
+//run and check it 
+
+
+//number type has a perticular range not sure but it is betweeen 2 of power 52
+//larger than it can be store in longint
